8ba8266cc050d40752e797faf7a22994c3564c0c
Sockshead <juancapoar@gmail.com>
2022-07-08 05:11:42 -0500

    tailwind configuration & home page

--- src/App.vue
+++ src/App.vue
@@ -24,95 +24,17 @@ import HelloWorld from "@/components/HelloWorld.vue";
 	</header>
 
 	<RouterView />-->
-
-	<img class="screen" src="@/assets/wellraum.jpeg" alt="Wellraum logo" />
+	<div
+		class="flex justify-center items-center h-screen overflow-y-hidden bg-[#f7f7f7]"
+	>
+		<div
+			class="absolute animate-pulse w-[50%] h-[95%] bg-current rounded-full"
+		/>
+		<div class="absolute animate-pulse w-[60%] h-40 bg-current rounded-2xl" />
+		<img
+			class="w-1/2 ml-auto mr-auto"
+			src="@/assets/wellraum.jpeg"
+			alt="Wellraum logo"
+		/>
+	</div>
 </template>
-
-<style>
-@import "@/assets/base.css";
-#app {
-	max-height: 100vh;
-	overflow-y: hidden;
-	background-color: #f7f7f7;
-	/*max-width: 1280px;
-	margin: 0 auto;
-	padding: 2rem;
-	font-weight: normal;*/
-}
-header {
-	line-height: 1.5;
-	max-height: 100vh;
-}
-.screen {
-	display: block;
-	max-height: 100vh;
-	margin-left: auto;
-	margin-right: auto;
-}
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-a,
-.green {
-	text-decoration: none;
-	color: hsla(160, 100%, 37%, 1);
-	transition: 0.4s;
-}
-@media (hover: hover) {
-	a:hover {
-		background-color: hsla(160, 100%, 37%, 0.2);
-	}
-}
-nav {
-	width: 100%;
-	font-size: 12px;
-	text-align: center;
-	margin-top: 2rem;
-}
-nav a.router-link-exact-active {
-	color: var(--color-text);
-}
-nav a.router-link-exact-active:hover {
-	background-color: transparent;
-}
-nav a {
-	display: inline-block;
-	padding: 0 1rem;
-	border-left: 1px solid var(--color-border);
-}
-nav a:first-of-type {
-	border: 0;
-}
-@media (min-width: 1024px) {
-	/*body {
-		display: flex;
-		place-items: center;
-	}
-	#app {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		padding: 0 2rem;
-	}*/
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-	nav {
-		text-align: left;
-		margin-left: -1rem;
-		font-size: 1rem;
-		padding: 1rem 0;
-		margin-top: 1rem;
-	}
-}
-</style>
