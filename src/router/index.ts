83b21a46e8d21a593154a3a23e6a9bf2ade07554
Sockshead <juancapoar@gmail.com>
2022-07-08 05:51:34 -0500

    router update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,18 @@
+import { defineAsyncComponent } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(), //createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: "/",
 			name: "home",
-			component: HomeView,
+			component: defineAsyncComponent(() => import("../views/HomeView.vue")),
 		},
 		{
 			path: "/about",
 			name: "about",
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import("../views/AboutView.vue"),
+			component: defineAsyncComponent(() => import("../views/AboutView.vue")),
 		},
 	],
 });
