8405af7195568567e5cccd37cbb5ceae94ec36fc
Sockshead <juancapoar@gmail.com>
2022-07-09 18:24:20 -0500

    responsiveness fix

--- src/App.vue
+++ src/App.vue
@@ -28,9 +28,11 @@
 		class="flex justify-center items-center h-screen overflow-y-hidden bg-[#f7f7f7]"
 	>
 		<div
-			class="absolute animate-pulse w-[50%] h-[95%] bg-current rounded-full"
+			class="absolute animate-pulse w-44 h-44 md:w-64 md:h-64 lg:w-1/2 lg:h-[95%] bg-current rounded-full"
 		/>
-		<div class="absolute animate-pulse w-[60%] h-40 bg-current rounded-2xl" />
+		<div
+			class="absolute animate-pulse w-[60%] h-16 md:h-24 lg:h-40 bg-current rounded-2xl"
+		/>
 		<img
 			class="w-1/2 ml-auto mr-auto"
 			src="@/assets/wellraum.jpeg"
